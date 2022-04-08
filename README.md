6ca896147ff823e1a3fe741b1887c93f9be9e9cd
Alfen Febral <alfen@codex.works>
2022-04-08 10:17:51 +0700

    Initial Commit

---
+++ README.md
@@ -0,0 +1,11 @@
+# Go MQTT Example
+Golang Example of MQTT
+## Usage
+run docker compose
+```bash
+docker compose up -d
+```
+and run go
+```bash
+go run main.go
+```
---
+++ main.go
@@ -0,0 +1,68 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"os"
+	"time"
+
+	mqtt "github.com/eclipse/paho.mqtt.golang"
+	"github.com/joho/godotenv"
+)
+
+var messagePubHandler mqtt.MessageHandler = func(client mqtt.Client, msg mqtt.Message) {
+	fmt.Printf("Received message: %s from topic: %s\n", msg.Payload(), msg.Topic())
+}
+
+var connectHandler mqtt.OnConnectHandler = func(client mqtt.Client) {
+	fmt.Println("Connected")
+}
+
+var connectLostHandler mqtt.ConnectionLostHandler = func(client mqtt.Client, err error) {
+	fmt.Printf("Connect lost: %v", err)
+}
+
+func sub(client mqtt.Client) {
+	topic := "topic/test"
+	token := client.Subscribe(topic, 1, nil)
+	token.Wait()
+	fmt.Printf("Subscribed to topic %s", topic)
+}
+
+func publish(client mqtt.Client) {
+	num := 10
+	for i := 0; i < num; i++ {
+		text := fmt.Sprintf("Message %d", i)
+		token := client.Publish("topic/test", 0, false, text)
+		token.Wait()
+		time.Sleep(time.Second)
+	}
+}
+
+func main() {
+	err := godotenv.Load()
+	if err != nil {
+		log.Fatal("Error loading .env file")
+	}
+
+	opts := mqtt.NewClientOptions()
+	opts.AddBroker(os.Getenv("MQTT_BROKER_URL"))
+	opts.SetKeepAlive(2 * time.Second)
+	opts.SetDefaultPublishHandler(messagePubHandler)
+	opts.SetPingTimeout(1 * time.Second)
+
+	opts.OnConnect = connectHandler
+	opts.OnConnectionLost = connectLostHandler
+
+	client := mqtt.NewClient(opts)
+	if token := client.Connect(); token.Wait() && token.Error() != nil {
+		panic(token.Error())
+	}
+
+	sub(client)
+	publish(client)
+
+	client.Disconnect(250)
+
+	time.Sleep(1 * time.Second)
+}
